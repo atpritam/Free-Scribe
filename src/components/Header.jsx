baf335cf24885ecb1eef95616734a618a151ec85
Pritam Chakraborty <pritam.amit29@gmail.com>
2024-07-31 03:24:38 +0200

    Fixed route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,12 @@ const Header = (props) => {
     const {isAudioAvailable} = props
     return (
         <header className='flex items-center justify-between gap-4 p-4'>
-            <a href="/"><h1 className='font-medium'>Free<span className='text-blue-400 bold'>Scribe</span></h1></a>
+            <a href="/Free-Scribe/"><h1 className='font-medium'>Free<span className='text-blue-400 bold'>Scribe</span></h1></a>
             {
                 isAudioAvailable  ? 
                 (
                 <div className='gap-4 flex items-center '>
-                    <a href="/" className='flex items-center gap-2 specialBtn px-3 py-2 rounded-lg text-blue-400'>
+                    <a href="/Free-Scribe/" className='flex items-center gap-2 specialBtn px-3 py-2 rounded-lg text-blue-400'>
                         <p>New</p>
                         <i className="fa-solid fa-plus"></i>
                     </a>
