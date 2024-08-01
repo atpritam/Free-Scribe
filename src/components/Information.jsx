c69f2b1a9d8e9859473ba5d30b50bb428d69249e
Pritam Chakraborty <pritam.amit29@gmail.com>
2024-08-01 14:27:33 +0200

    Rfd 3.d1

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -41,7 +41,7 @@ function Information(props) {
     };
 
     return (
-        <main className='flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 justify-center pb-20 w-full max-w-prose mx-auto mt-10'>
+        <main className='flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 justify-center pb-20 w-full max-w-fit mx-auto mt-10'>
             <h1 className='font-semibold text-3xl sm:text-5xl md:text-6xl'>Your <span className='text-blue-400 bold nowrap'>{tab}</span></h1>
             <div className='grid grid-cols-2 sm:mx-auto bg-white rounded overflow-hidden items-center p-1 blueShadow border-[2px] border-solid border-blue-300'>
                 <button className={'px-4 rounded duration-200 py-1 ' + (tab === 'Transcription' ? ' bg-blue-300 text-white' : ' text-blue-400 hover:text-blue-600')}
